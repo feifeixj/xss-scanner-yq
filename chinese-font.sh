3b4af67671e937581df75d12aada33f2c8b267bd
feifeixj <870487116@qq.com>
2018-05-29 15:28:14 +0800

    Update chinese-font.sh

--- chinese-font.sh
+++ chinese-font.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #安装中文字体
+yum -y install fontconfig
 wget http://sourceforge.net/projects/wqy/files/wqy-microhei/0.2.0-beta/wqy-microhei-0.2.0-beta.tar.gz
 tar -zvxf wqy-microhei-0.2.0-beta.tar.gz 
 mkdir -p /usr/share/fonts/wenquanyi  
